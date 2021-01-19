87c8a92c4ec3dce7d27fbf85f240fad52f9fc0d0
GGgun2013 <gggun2013@gmail.com>
2021-01-19 16:24:15 +0700

    lab12_1clear

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,26 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N, double B[])
+{
+    double sum,sumstd = 0,sumhar,sumgeo = 1,Max = A[0],Min = A[0],Ni;
+    for(int i=0;i < N;i++)
+    {
+        sum += A[i];
+        sumgeo *= A[i];
+        sumhar += 1/A[i];
+        if(A[i]>Max) Max=A[i];
+        if(A[i]<Min) Min=A[i]; 
+    }
+    B[0] = sum/N;
+    for(int i=0;i < N;i++)
+    {
+    sumstd +=(pow(A[i],2)-pow(B[0],2));
+    }
+    B[1] = sqrt(1.0/N*sumstd);
+    B[2] = pow(sumgeo,1.0/N);
+    B[3] = N/sumhar;
+    B[4] = Max;
+    B[5] = Min;
+}
